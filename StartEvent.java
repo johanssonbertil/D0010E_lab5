7ae8de73ef9f239ef5ad4bb1fe5f2a8da5f4a9e0
ItzCornflakez <61742834+ItzCornflakez@users.noreply.github.com>
2020-03-03 14:33:14 +0100

    Update StartEvent.java

--- StartEvent.java
+++ StartEvent.java
@@ -1,8 +1,14 @@
 
 public class StartEvent extends Event {
+	
+	private State state;
+	
+	public StartEvent(State state, int time) {
+		super(state, time);
+	}
 
 	public void doEvent() {
-		ShopState.running = true;
+		state.setRunning(true);
 	}
 	
 }
