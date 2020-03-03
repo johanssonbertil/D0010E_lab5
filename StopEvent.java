00d7abbdeff7be372c442e98b026777311673c94
ItzCornflakez <61742834+ItzCornflakez@users.noreply.github.com>
2020-03-03 15:28:02 +0100

    Update StopEvent.java

--- StopEvent.java
+++ StopEvent.java
@@ -1,9 +1,6 @@
 
+
 public class StopEvent extends Event {
-	
-	private State state;
-	
-	private EventQueue eventQueue;
 	
 	public StopEvent(State state, int time, EventQueue eventQueue){
 		super(state, time, eventQueue);
