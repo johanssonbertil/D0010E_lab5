90061c3aaf86ca5ae5a14d54f7b36400d4dba66b
ItzCornflakez <61742834+ItzCornflakez@users.noreply.github.com>
2020-03-03 14:34:37 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -3,10 +3,17 @@ public abstract class Event {
 	
 	private double time;
 	
+	private State state;
 	
+	public Event(State state, int time){
+		this.state = state;
+		this.time = time;
+	}
+
 	public abstract void doEvent();
 	
 	public double getTime() {
 		return this.time;
 	}
 }
+
