97ed2c897d946eaf315d0ea0aea341268e3529ad
johanssonbertil <51829814+johanssonbertil@users.noreply.github.com>
2020-03-03 13:50:51 +0100

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,17 @@
 
 public class Customer {
-
+	private int customerID;
+	private int arrivalTime;
+	
+	Customer(int customerID){
+		this.customerID = customerID;
+	}
+	
+	public int getID() {
+		return customerID;
+	}
+	
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
 }
