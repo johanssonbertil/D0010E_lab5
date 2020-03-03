d1640e9e6e7d97b3a5d886134a1cda48d65a3b59
ItzCornflakez <61742834+ItzCornflakez@users.noreply.github.com>
2020-03-03 15:27:31 +0100

    Update StartEvent.java

--- StartEvent.java
+++ StartEvent.java
@@ -1,9 +1,6 @@
 
+
 public class StartEvent extends Event {
-	
-	private State state;
-	
-	private EventQueue eventQueue;
 	
 	public StartEvent(State state, int time, EventQueue eventQueue) {
 		super(state, time, eventQueue);
@@ -14,3 +11,4 @@ public class StartEvent extends Event {
 	}
 	
 }
+
